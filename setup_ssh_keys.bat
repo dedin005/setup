ab325b68f7bbb1c010e9f8d665c3cb37b1ef3f5a
Charlie Dedinsky <dedin005@umn.edu>
2023-01-18 12:29:08 -0600

    finish .bat

--- setup_ssh_keys.bat
+++ setup_ssh_keys.bat
@@ -16,8 +16,10 @@ rem ssh-copy-id -i %USERPROFILE%\.ssh\id_ed25519.pub %X500%@%LAB_MACHINE%
 scp %USERPROFILE%\.ssh\id_ed25519.pub %X500%@%LAB_MACHINE%:~/.ssh/authorized_keys
 
 rem Create the ssh config file
-set "configfile=Host %LAB_MACHINE%"
-set "configfile=%configfile%\n^  User %X500%"
-set "configfile=%configfile%\n^  IdentityFile %USERPROFILE%\.ssh\id_ed25519"
+set "configfile0=Host %LAB_MACHINE%"
+set "configfile1=  User %X500%"
+set "configfile2=  IdentityFile %USERPROFILE%\.ssh\id_ed25519"
 
-echo %configfile% > %USERPROFILE%\.ssh\config
+echo %configfile0% > %USERPROFILE%\.ssh\config
+echo %configfile1% >> %USERPROFILE%\.ssh\config
+echo %configfile2% >> %USERPROFILE%\.ssh\config
